9479c3ee7c74c5111cef6d8edd353cf35b58fe59
PranavNarayan7034 <139863209+PranavNarayan7034@users.noreply.github.com>
2024-12-30 10:16:49 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,4 +1,7 @@
 from pyhive import hive
+
+from Hive_project.file2 import bar_width
+
 # Connection to hive
 db = 'batch85'
 crs = hive.connect(host='localhost',database=db).cursor()
@@ -23,8 +26,6 @@ try:
         crs.execute(f"load data inpath '{filepath}' overwrite into table Online_sales")
         print('Data loaded Successfully')
     else:
-        # filepath  = 'hdfs://localhost:9000/user/hive/warehouse/batch85.db/online_sales/online_sales_dataset.csv'
-        # crs.execute(f"load data inpath '{filepath}' overwrite into table Online_sales")
         print('Data is already Available in table')
 
 except Exception:
@@ -41,16 +42,17 @@ out = crs.fetchall()
 x_column = 'country'
 crs.execute(f'select distinct {x_column} from Online_sales')
 x = crs.fetchall()
+print(f'x are {x}')
 
 import numpy as np
 x_values = np.arange(1,len(x)+1)
-# print(x_values)
+print(f'x_values are {x_values}')
 
 BT = []
 CC = []
 PP = []
 for i in out:
-    print(i)
+    # print(i)
     if i[1] == 'Bank Transfer':
         BT.append(i[2])
     elif i[1] == 'Credit Card':
@@ -63,3 +65,20 @@ print(x)
 print(BT)
 print(CC)
 print(PP)
+
+#Bar graph
+import matplotlib.pyplot as plt   #pip install matplotlib
+
+bar_width = 0.25
+plt.figure(figsize=(12,6))
+plt.bar(x_values-bar_width,BT,bar_width,color='red',label='Bank Transactions')
+plt.bar(x_values,CC,bar_width,label='Credit Card')
+plt.bar(x_values+bar_width,PP,bar_width,label='Paypal')
+
+plt.xticks(x_values,x)
+plt.title('No.of payment methods in each Countries')
+plt.xlabel('Countries')
+plt.ylabel('Count')
+plt.legend()
+
+plt.show()
